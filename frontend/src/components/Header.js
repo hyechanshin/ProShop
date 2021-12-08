dc23d51df7aca001fdde22957cb5158ac46e50df
Hyechan Shin <kirikeria@gmail.com>
2021-12-08 14:43:26 +0200

    HomeScreen Product Listing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,8 +10,8 @@ function Header() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/cart">Cart</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                        <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
