aa10da955d80e0a5494f8bef42e1c1a912044f64
Hyechan Shin <kirikeria@gmail.com>
2021-12-09 22:55:19 +0200

    fix routing

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 function ProductScreen() {
+    const { id } = useParams();
+    const product = products.find((p) => p._id == Number(id))
     return (
         <div>
-            Product
+            {product.name}
         </div>
     )
 }
