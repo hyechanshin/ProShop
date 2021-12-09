6d1b004f5e14d17eb074953869670cf08bd89218
Hyechan Shin <kirikeria@gmail.com>
2021-12-09 11:10:10 +0200

    fix Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom'
 function Product({ product }) {
     return (
         <Card className="my-3 p-3 rounded">
-            <Link to={'/product/${product._id}'}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} />
             </Link>
 
             <Card.Body>
-                <Link to={'/product/${product._id}'}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as="div">
                         <strong>{product.name}</strong>
                     </Card.Title>
